8448355cd0fb95107fedb324a3d09ea5930b061f
maxmuehle <maximilian.muehlfeld@wtl.de>
2019-07-11 12:03:27 +0200

    cleanup test

--- src/__tests__/SuggestionInput.test.tsx
+++ src/__tests__/SuggestionInput.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import {SuggestionInput} from '../example/SuggestionInput';
-import {act} from 'react-dom/test-utils';
 
 
 const getDummyEvent = (value: string) => {
